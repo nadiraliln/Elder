d6c43dcb748da8eb1493d088f27b465a782bdc0b
Nadir Ali <nadirali7q12@gmail.com>
2025-07-04 17:49:20 +0500

    Update app.py

--- app.py
+++ app.py
@@ -47,5 +47,5 @@ def serve_file(filename):
 
     return send_from_directory(DOWNLOAD_DIR, filename, as_attachment=True)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
